5035011d928b3e31a4ae152f864fa278d6e92c3d
Michal Morawiec <yMichaelmorawiec@wp.pl>
2024-01-14 17:05:39 +0100

    New net introduced

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Torch_cpp
+
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+unzip libtorch-shared-with-deps-latest.zip
+
+cmake -DCMAKE_PREFIX_PATH=..(your absolute path)/Torch_cpp/libtorch
+
+cmake --build . --config Release
--- torch_app.cpp
+++ torch_app.cpp
@@ -1,8 +1,30 @@
 #include <torch/torch.h>
 #include <iostream>
 
+class Net : public torch::nn::Module 
+{
+    public:
+        Net(): conv1(register_module("conv1", torch::nn::Conv2d(torch::nn::Conv2dOptions(1, 10, 5)))),
+                conv2(register_module("conv2", torch::nn::Conv2d(torch::nn::Conv2dOptions(10, 10, 5)))),
+                fc1(register_module("fc1", torch::nn::Linear(320,50))),
+                fc2(register_module("fc2", torch::nn::Linear(50,10)))
+        {}
+    
+    private:
+        torch::nn::Conv2d conv1;
+        torch::nn::Conv2d conv2;
+        torch::nn::Linear fc1;
+        torch::nn::Linear fc2;
+
+
+};
+
 int main()
 {
     torch::Tensor tensor = torch::rand({2,3});
     std::cout << tensor << std::endl;
+
+    Net myNetwork = Net();
+    
+    std::cout << myNetwork.name() <<std::endl;
 }
