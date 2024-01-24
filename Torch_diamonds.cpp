f52cd283a0b9dd2e9f2ee2cf579542c7e8670055
Michal Morawiec <yMichaelmorawiec@wp.pl>
2024-01-24 20:23:57 +0100

    Some loop training and testing change

--- Torch_diamonds.cpp
+++ Torch_diamonds.cpp
@@ -6,15 +6,25 @@
 class Net : public torch::nn::Module 
 {
     public:
-        Net()
+        Net():
+            fc1(register_module("fc1", torch::nn::Linear(3,64))),
+            fc2(register_module("fc2", torch::nn::Linear(64,32))),
+            fc3(register_module("fc3", torch::nn::Linear(32,1)))
         {
         }
         torch::Tensor forward(torch::Tensor x)
         {
+
+            x = torch::tanh(fc1->forward(x));
+            x = torch::tanh(fc2->forward(x));
+            x = fc3->forward(x);
+
             return x;
         }
     private:
-
+        torch::nn::Linear fc1;
+        torch::nn::Linear fc2;
+        torch::nn::Linear fc3;
 };
 
 class DiamondsDataset : public torch::data::datasets::Dataset<DiamondsDataset>
@@ -30,9 +40,10 @@ class DiamondsDataset : public torch::data::datasets::Dataset<DiamondsDataset>
         {
             float carat = std::stoi(content_diamonds_dataset[index][0]);
             float depth = std::stoi(content_diamonds_dataset[index][4]);
+            float table = std::stoi(content_diamonds_dataset[index][5]);
             float target = std::stoi(content_diamonds_dataset[index][6]);
             
-            return {torch::tensor({carat, depth}), torch::tensor({target})};
+            return {torch::tensor({carat, depth, table}), torch::tensor({target})};
         }
 
         torch::optional<size_t> size() const
@@ -74,13 +85,44 @@ class DiamondsDataset : public torch::data::datasets::Dataset<DiamondsDataset>
         }
 };
 
-
 int main()
 {   
     std::cout << "Diamonds..." << std::endl;
+    Net myNetwork = Net();
     DiamondsDataset dataset;
     auto example = dataset.get(0);
     std::cout << example.data << std::endl;
     std::cout << example.target << std::endl;
+    
+    torch::optim::SGD optimizer(myNetwork.parameters(), torch::optim::SGDOptions(0.01));
+    torch::nn::MSELoss criterion;
+
+    //Train loop
+    for (int i = 0; i< 500; ++i)
+    {
+        example = dataset.get(i);
+        optimizer.zero_grad();
+
+        torch::Tensor prediction  = myNetwork.forward(example.data);
+        torch::Tensor loss = criterion(prediction, example.target);
+
+        loss.backward();
+        optimizer.step();
+        
+        if (i % 50 == 0)
+        {
+            std::cout << "Epoch: " << i << " Loss: " << loss.item<float>() << std::endl;
+        }
+        
+    }
+    
+    //Test loop
+    for (int i = 600; i< 650; ++i)
+    {
+        example = dataset.get(i);
+        
+        torch::Tensor prediction  = myNetwork.forward(example.data);
+        std::cout << "Prediction: " << prediction << std::endl;
+    }
     return 0;
 }
